18ea2ea97a01a4572073cfc871cecd8e945a3187
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-06-02 09:47:39 +0700

    update

--- react/src/assets/icons/index.ts
+++ react/src/assets/icons/index.ts
@@ -1,12 +1,13 @@
-export { default as IconMessage} from '@assets/icons/message.svg?react';
-export { default as IconRight} from '@assets/icons/right.svg?react';
-export { default as IconEye} from '@assets/icons/eye.svg?react';
+export { default as IconCalender } from '@assets/icons/calender.svg?react';
+export { default as IconChecked } from '@assets/icons/checked.svg?react';
+export { default as IconChevronLeft } from '@assets/icons/chevron-left.svg?react';
+export { default as IconChevronRight } from '@assets/icons/chevron-right.svg?react';
+export { default as IconClear } from '@assets/icons/clear.svg?react';
+export { default as IconClose } from '@assets/icons/close.svg?react';
+export { default as IconEyeClose } from '@assets/icons/eye-close.svg?react';
+export { default as IconEye } from '@assets/icons/eye.svg?react';
+export { default as IconMessage } from '@assets/icons/message.svg?react';
+export { default as IconPaymentMethodVisa } from '@assets/icons/payment-method-visa.svg?react';
+export { default as IconPercentage } from '@assets/icons/percentage.svg?react';
+export { default as IconRight } from '@assets/icons/right.svg?react';
 
-export { default as IconEyeClose} from '@assets/icons/eye-close.svg?react';
-export { default as IconPercentage} from '@assets/icons/percentage.svg?react';
-export { default as IconCalender} from '@assets/icons/calender.svg?react';
-export { default as IconClear} from '@assets/icons/clear.svg?react';
-export { default as IconPaymentMethodVisa} from '@assets/icons/payment-method-visa.svg?react';
-export { default as IconChevronLeft} from '@assets/icons/chevron-left.svg?react';
-export { default as IconChevronRight} from '@assets/icons/chevron-right.svg?react';
-export { default as IconChecked} from '@assets/icons/checked.svg?react';
