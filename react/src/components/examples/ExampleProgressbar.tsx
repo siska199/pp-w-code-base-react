467d073df4f1273509f05e1717795e5718145e5a
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-06-06 05:52:41 +0700

    fix styling progress step vertical

--- react/src/components/examples/ExampleProgressbar.tsx
+++ react/src/components/examples/ExampleProgressbar.tsx
@@ -3,8 +3,7 @@ import Progressbar from "@components/ui/Progressbar"
 
 const ExampleProgressbar = () => {
     const activeIndexStep = 1
-
-    const listStepVertical = [
+    const listStepHorizontal = [
         {
             title: "Get Started",
             caption: "Let us know little bit more about what you do."
@@ -30,12 +29,51 @@ const ExampleProgressbar = () => {
             caption: "Let us know little bit more about what you do."
         }
     ]
-    
+
+    const listStepVertical = [
+        {
+            title: "Get Started",
+            caption:<div className="h-[10rem] border">
+
+            </div>
+        },
+        {
+            title: "Get Started",
+            caption:<div className="h-[10rem] border">
+
+            </div>
+        },
+        {
+            title: "Get Started",
+            caption: <div className="h-[10rem] border">
+
+            </div>
+        },
+        {
+            title: "Get Started",
+            caption: <div className="h-[10rem] border">
+
+            </div>
+        },
+        {
+            title: "Get Started",
+            caption: <div className="h-[10rem] border">
+
+            </div>
+        },
+        {
+            title: "Get Started",
+            caption: <div className="h-[10rem] border">
+
+            </div>
+        }
+    ]
+
     return (
         <div className="w-full border p-[5rem] flex flex-col gap-4">
             <Progressbar value={50} valueTotal={100} size="small" />
             <Progressbar value={50} valueTotal={100} />
-            <ProgressStep activeStep={activeIndexStep} listStep={listStepVertical} />
+            <ProgressStep activeStep={activeIndexStep} listStep={listStepHorizontal} />
             <ProgressStep activeStep={activeIndexStep} listStep={listStepVertical} variant="vertical" />
         </div>
     )
