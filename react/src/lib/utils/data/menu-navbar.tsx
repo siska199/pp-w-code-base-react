e11a8861fd13572b2f649d5157bb196110296b6d
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-06-12 10:41:43 +0700

    fix redirect menu

---
+++ react/src/lib/utils/data/menu-navbar.tsx
@@ -0,0 +1,21 @@
+const listMenuNavbar = [
+    {
+        label: 'Docs',
+        url: '/docs'
+    },
+    {
+        label: 'Components',
+        url: '/docs/components/accordion'
+    },
+    {
+        label: 'Examples',
+        url: '/docs/components/examples'
+    },
+    {
+        label: 'Github',
+        url: 'https://github.com/siska199/my-code-base-199'
+    }
+]
+
+
+export default listMenuNavbar
