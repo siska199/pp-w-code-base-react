456ea5c58bbd338981c1f63971f15abab913008e
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-06-12 19:37:02 +0700

    fix component badge

--- react/src/components/module/badge-page/CardBadgeIntro.tsx
+++ react/src/components/module/badge-page/CardBadgeIntro.tsx
@@ -3,11 +3,29 @@ import CardIntroComponent from "@components/cards/CardIntroComponent";
 import Badge from "@components/ui/Badge";
 
 const CardBadgeIntro = () => {
+  const listExample = [
+    {
+      component: <div className="flex gap-2 items-center">
+        <Badge label="SISKA" variant={"soft-warning"} sizeBadge={"small"} />
+        <Badge label="SISKA" variant={"soft-border-success"} />
+        <Badge label="SISKA" variant={"solid-black"} />
+        <Badge label="SISKA" variant={"outline-error"} sizeBadge={"large"} shapeBadge={"rounded"} />
+      </div>
+    },
+    {
+      component: <div className="flex gap-2 items-center">
+        <Badge label="1" shapeBadge={"circle"} variant={"soft-border-primary"} />
+        <Badge label="100" shapeBadge={"circle"} variant={"soft-border-primary"} />
+        <Badge label="10000" shapeBadge={"circle"} variant={"soft-border-primary"} />
+        <Badge label="100000" shapeBadge={"circle"} variant={"soft-border-primary"} />
+      </div>
+    }
+  ]
   return (
     <CardIntroComponent
       title={'Badge'}
       subTitle="Description of Badge."
-      DisplayComponent={<Badge />}
+      listExample={listExample}
       displayCodeBase={displayCodeBase}
     />
   );
---
+++ react/src/lib/utils/data/variant-color.tsx
@@ -0,0 +1,35 @@
+const variant = {
+    "solid-black": "bg-black",
+    "solid-gray": "bg-gray",
+    "solid-primary": "bg-primary",
+    "solid-success": "bg-success",
+    "solid-warning": "bg-warning",
+    "solid-error": "bg-error",
+    "solid-white": "bg-white",
+
+    "soft-black": "bg-black-50 text-black-700",
+    "soft-gray": "bg-gray-50 text-gray-700",
+    "soft-primary": "bg-primary-50 text-primary-700",
+    "soft-success": "bg-success-50 text-success-700",
+    "soft-warning": "bg-warning-50 text-warning-700",
+    "soft-error": "bg-error-50 text-error-700",
+    "soft-white": "bg-white-50 text-white-700",
+
+    "soft-border-black": "bg-black-50 text-black-700 border-black border",
+    "soft-border-gray": "bg-gray-50 text-gray-700 border-gray border",
+    "soft-border-primary": "bg-primary-50 text-primary-700 border-primary border",
+    "soft-border-success": "bg-success-50 text-success-700 border-success border",
+    "soft-border-warning": "bg-warning-50 text-warning-700 border-warning border",
+    "soft-border-error": "bg-error-50 text-error-700 border-error border",
+    "soft-border-white": "bg-white-50 text-white-700 border-white border",
+
+    "outline-black": "text-black border-black border",
+    "outline-gray": "text-gray border-gray border",
+    "outline-primary": "text-primary border-primary border",
+    "outline-success": "text-success border-success border",
+    "outline-warning": "text-warning border-warning border",
+    "outline-error": "text-error border-error border",
+    "outline-white": "text-white border-white border"
+}
+
+export default variant
