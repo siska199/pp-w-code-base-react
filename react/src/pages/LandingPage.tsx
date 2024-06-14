f75274747f326b9634a42a82acb8164f5c716f33
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-06-14 11:44:17 +0700

    update

--- react/src/pages/LandingPage.tsx
+++ react/src/pages/LandingPage.tsx
@@ -9,7 +9,7 @@ const LandingPage = () => {
                     <br />One Codebase at a Time.
                 </h4>
                 <p className="text-gray text-body-large text-center">
-                    Offering a vast array of customizable code snippets and detailed guidelines, Codebase 199 revolutionizes frontend development, enabling developers to create robust and scalable applications with ease and efficiency.                </p>
+                   Codebase 199 revolutionizes frontend development, enabling developers to create robust and scalable applications with ease and efficiency.                </p>
                 <div className="flex w-full md:w-auto gap-4 mt-4">
                     <Button customeElement={"link"} variant={"solid-primary"} className="min-w-[7rem]" to={"/docs"} label={'Get Sarted'} />
                     <Button customeElement={"link"} variant={"solid-white"} className="min-w-[7rem]" to={"https://github.com/siska199/my-code-base-199"} label={'Github'} target="_blank" />
