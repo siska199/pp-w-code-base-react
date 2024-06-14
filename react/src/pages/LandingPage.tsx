a3a04604dec1e6a32244844e4dd3934aa4a919fb
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-06-14 11:21:16 +0700

    update

--- react/src/pages/LandingPage.tsx
+++ react/src/pages/LandingPage.tsx
@@ -5,10 +5,11 @@ const LandingPage = () => {
         <article className="h-[75vh] flex items-center">
             <div className="w-full md:w-[50rem] flex flex-col justify-center items-center mx-auto my-auto ">
                 <h4 className="font-bold text-center ">
-                    Streamline Your Frontend Development Process
+                    Empowering Frontend Developers,
+                    <br />One Codebase at a Time.
                 </h4>
                 <p className="text-gray text-body-large text-center">
-                    Building a robust and scalable frontend codebase has never been easier. Codebase 199 provides developers with the tools and guidelines necessary to create a strong foundation for any frontend project.                </p>
+                    Offering a vast array of customizable code snippets and detailed guidelines, Codebase 199 revolutionizes frontend development, enabling developers to create robust and scalable applications with ease and efficiency.                </p>
                 <div className="flex w-full md:w-auto gap-4 mt-4">
                     <Button customeElement={"link"} variant={"solid-primary"} className="min-w-[7rem]" to={"/docs"} label={'Get Sarted'} />
                     <Button customeElement={"link"} variant={"solid-white"} className="min-w-[7rem]" to={"https://github.com/siska199/my-code-base-199"} label={'Github'} target="_blank" />
