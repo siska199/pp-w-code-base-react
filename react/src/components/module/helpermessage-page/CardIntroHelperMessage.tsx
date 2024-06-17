c80fe319abf23598461068ee64b9590f2cb79c30
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-06-17 16:00:47 +0700

    add code based for helper mesage

--- react/src/components/module/helpermessage-page/CardIntroHelperMessage.tsx
+++ react/src/components/module/helpermessage-page/CardIntroHelperMessage.tsx
@@ -39,6 +39,30 @@ const CardIntroHelperMessage = () => {
   );
 }
 
-const displayCodeBase = `// Code for HelperMessage`;
+const displayCodeBase = `import clsx from "clsx";
+import { HTMLAttributes } from "react";
+
+
+interface TProps extends HTMLAttributes<HTMLParagraphElement>{
+    message? : string;
+    variant:"error"|"success"|"warning"
+}
+const HelperMessage = (props:TProps) => {
+    const {message,variant,className, ...attrs} = props
+
+  return (
+    message ? <p className={clsx({
+      [className||""]:className,
+      "text-body-small":true,
+      "text-error" : variant==="error",
+      "text-warning" : variant==="warning",
+      "text-success" : variant==="success",
+    })} {...attrs}>
+      {message}
+    </p> : null
+  )
+}
+
+export default HelperMessage`;
 
 export default CardIntroHelperMessage;
