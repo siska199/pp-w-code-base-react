3a6bcdc11ddbaf29f3d019b165d8224fbdccebed
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-06-22 10:13:41 +0700

    update variant structure

--- react/src/lib/utils/variants/ui/variant-alert.ts
+++ react/src/lib/utils/variants/ui/variant-alert.ts
@@ -1,4 +1,21 @@
-import variant, { variantAlertError, variantAlertSuccess, variantAlertWarning } from "@lib/utils/variants/variant-color"
+import variant from "@lib/utils/variants/variant-color"
+
+export const variantAlertError = {
+    "error-solid": variant["solid-error"],
+    "error-soft": variant["soft-error"],
+    "error-outline": variant["outline-error"],
+}
+export const variantAlertSuccess = {
+    "success-solid": variant["solid-success"],
+    "success-soft": variant["soft-success"],
+    "success-outline": variant["outline-success"],
+}
+export const variantAlertWarning = {
+    "warning-solid": variant["solid-warning"],
+    "warning-soft": variant["soft-warning"],
+    "warning-ouline": variant["outline-warning"],
+}
+
 
 const variantsAlert = {
     variant: {
---
+++ react/src/lib/utils/variants/ui/variant-badge.ts
@@ -0,0 +1,14 @@
+import variant from "@lib/utils/variants/variant-color"
+
+const variantBadge = {
+    ...variant,
+    "softborder-black": "bg-gray-100 text-gray-900 border-black border",
+    "softborder-gray": "bg-gray-50  text-gray-700 border-gray border",
+    "softborder-primary": "bg-primary-50 text-primary-700 border-primary border",
+    "softborder-success": "bg-success-50 text-success-700 border-success border",
+    "softborder-warning": "bg-warning-50 text-warning-700 border-warning border",
+    "softborder-error": "bg-error-50 text-error-700 border-error border",
+    "softborder-white": "bg-white-50 text-white-700 border-white border",
+}
+
+export default variantBadge
