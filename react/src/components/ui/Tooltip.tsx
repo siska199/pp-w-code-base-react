8b2edb5cb58712022078c1a902203e3c857e25a9
SPF <SPF@SM2HONB17D33SPF>
2024-07-04 15:13:54 +0700

    fix tooltip styling

--- react/src/components/ui/Tooltip.tsx
+++ react/src/components/ui/Tooltip.tsx
@@ -1,15 +1,19 @@
 interface TProps {
     children: React.ReactNode;
     text: string;
-    variant?: "top" | "bottom" | "left" | "right"
+    variant?: "top" | "bottom" | "left" | "right";
+    customeClass :{
+        tooltip:string;
+        rectangle:string;
+    }
 }
 
 const Tooltip = (props: TProps) => {
-    const { children, text, variant = "top" } = props
+    const { children, text, variant = "top",customeClass  } = props
     return (
-        <div data-text={text} className={`tooltip ${variant} relative flex justify-center items-center w-fit p-1`}>
+        <div data-text={text} className={`tooltip ${customeClass ?.tooltip} ${variant} relative flex justify-center items-center w-fit p-1`}>
             {children}
-            <div className={`rectangle ${variant}`}></div>
+            <div className={`rectangle ${variant}  ${customeClass ?.rectangle}`}></div>
         </div>
     )
 }
