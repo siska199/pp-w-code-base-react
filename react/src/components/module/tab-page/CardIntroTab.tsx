f8537ed0676f8554871d9ec39b486f93f848ea00
SPF <SPF@SM2HONB17D33SPF>
2024-07-04 15:48:05 +0700

    update content comp tab

--- react/src/components/module/tab-page/CardIntroTab.tsx
+++ react/src/components/module/tab-page/CardIntroTab.tsx
@@ -1,24 +1,61 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 
+import CardVariousCompVariant from "@components/CardVariousCompVariant";
 import CardIntroComponent from "@components/cards/CardIntroComponent";
+import Tabs from "@components/ui/Tabs";
+import codeStringComponentUi from "@lib/utils/code-string/component-ui";
 
 const CardIntroTab = () => {
 
-    const listExample = [
+  const variantTab = {
+    "vertical-bordered":{},
+    "vertical-pilled":{},
+    "horizontal-bordered":{},
+    "horizontal-pilled":{},
+  }
+
+  const listTab = [
     {
-      component: ""
+      id: "0",
+      label:"Tab 1",
+      content :<div className="w-full p-4 h-[10rem] rounded-md  border">
+        SISKA APRIANA RIFIANTI TAB CONTENT 1
+      </div>
+    },
+    {
+      id: "1",
+      label:"Tab 2",
+      content :<div className="w-full p-4 h-[10rem] rounded-md  border">
+        SISKA APRIANA RIFIANTI TAB CONTENT 2
+      </div>
+    }
+  ]
+
+  const listExample = [
+    {
+      component: <CardVariousCompVariant<typeof variantTab>
+        variant={variantTab}
+        groups={["horizontal","vertical"]}
+        Component={(variant, group) =>{
+          const type = variant?.split('-')[1]
+         // @ts-ignore
+          return  <Tabs layout={group} type={type} listTab={listTab}/>
+        }}
+        isLogicSplitGroupKey={true}
+        withBorder={false}
+      />
     },
 
   ]
   return (
     <CardIntroComponent
       title={'Tab'}
-      subTitle="Description of Tab."
+      subTitle="A component tab is a user interface element used in applications, websites, or graphical user interfaces (GUIs) to organize and display content in a structured and accessible manner. Tabs are typically found in navigation bars, toolbars, or within specific sections of an interface where content can be logically grouped and switched between by users."
       listExample={listExample }
-      displayCodeBase={displayCodeBase}
+      displayCodeBase={codeStringComponentUi.Tabs}
     />
   );
 }
 
-const displayCodeBase = `// Code for Tab`;
 
 export default CardIntroTab;
