a4090491bcf0862606d81d998dff561fb08cfdf6
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-07-05 17:11:46 +0700

    update

--- react/src/pages/ExamplesPage.tsx
+++ react/src/pages/ExamplesPage.tsx
@@ -1,9 +1,7 @@
-import ExampleTable from "@components/examples/ExampleTable";
 
 const ExamplesPage = () => {
   return (
     <div className='border p-4 w-full flex justify-center items-center flex-col'>
-      <ExampleTable />
     </div>
   );
 };
