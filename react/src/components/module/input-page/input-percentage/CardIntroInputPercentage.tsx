3bd8b5968d9eb160b3d834129ee8163bd5081686
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-07-10 12:49:32 +0700

    update InputPercentage validation

--- react/src/components/module/input-page/input-percentage/CardIntroInputPercentage.tsx
+++ react/src/components/module/input-page/input-percentage/CardIntroInputPercentage.tsx
@@ -1,4 +1,3 @@
-
 import CardIntroComponent from "@components/cards/CardIntroComponent";
 import CardVariantComp from "@components/cards/CardVariantComp";
 import InputPercentage from "@components/ui/inputs/InputPercentage";
@@ -6,34 +5,27 @@ import useForm from "@hooks/useForm";
 import codeStringComponentUi from "@lib/utils/code-string/component-ui";
 
 const CardIntroInputPercentage = () => {
-  const { form, handleOnChange } = useForm({
+  const { form, handleOnChange } = useForm<"input-percentage">({
     initialForm: {
-      'input-percentage': {}
-    }
-  })
+      "input-percentage": {
+        value: "",
+      },
+    },
+  });
 
   const listExample = [
     {
-      component: <CardVariantComp
-        title={'default '}
-        withBorder={false}
-        Component={<InputPercentage
-          onChange={handleOnChange}
-          {...form['input-percentage']}
-        />}
-      />
+      component: <CardVariantComp title={"default "} withBorder={false} Component={<InputPercentage onChange={handleOnChange} {...form["input-percentage"]} />} />,
     },
-
-  ]
+  ];
   return (
     <CardIntroComponent
-      title={'Input Percentage'}
+      title={"Input Percentage"}
       subTitle="An InputPercentage component is a specialized input field that allows users to enter a percentage value. It typically includes validation to ensure the input is within the acceptable range of 0 to 100%. This component simplifies user interactions by automatically appending the percentage symbol and handling any necessary formatting."
       listExample={listExample}
       displayCodeBase={codeStringComponentUi.InputPercentage}
     />
   );
-}
-
+};
 
 export default CardIntroInputPercentage;
--- react/src/components/ui/inputs/InputPercentage.tsx
+++ react/src/components/ui/inputs/InputPercentage.tsx
@@ -1,48 +1,43 @@
-import { TBasePropsInput } from '@/types/ui/index';
-import { IconPercentage } from '@assets/icons';
+import { TBasePropsInput } from "@/types/ui/index";
+import { IconPercentage } from "@assets/icons";
 import ContainerInput from "@components/ui/inputs/ContainerInput";
-import React from 'react';
+import React from "react";
 
 interface TProps extends TBasePropsInput, React.HTMLProps<HTMLInputElement> {
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    name: string;
-    value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  name: string;
 }
 
 const InputPercentage = (props: TProps) => {
-    const { onChange: handleOnChange, value, ...attrs } = props;
+  const { onChange: handleOnChange, value, ...attrs } = props;
 
-    const handleOnChangeFormatedValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-        let valueFormatted = e.target.value;
-        valueFormatted = valueFormatted
-            .replace(/[^\d.]+/g, "")
-            .replace(/(\..*?)\./g, "$1")
-            .replace(/(\.\d\d)\d+/g, "$1")
-            .replace(/^0+(?=\d)/, '');
-            
-        if ((parseFloat(valueFormatted) < 0 || parseFloat(valueFormatted) > 100) && valueFormatted !== "") {
-            valueFormatted = value
-        }
-        e.target.value = valueFormatted
-        handleOnChange(e)
+  const handleOnChangeFormatedValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let valueFormatted: string = e.target.value;
+    valueFormatted = valueFormatted
+      .replace(/[^\d.]+/g, "")
+      .replace(/(\..*?)\./g, "$1")
+      .replace(/(\.\d\d)\d+/g, "$1")
+      .replace(/^0+(?=\d)/, "");
+
+    if ((parseFloat(valueFormatted) < 0 || parseFloat(valueFormatted) > 100) && valueFormatted !== "") {
+      valueFormatted = String(value);
     }
-    return (
-        <ContainerInput<React.HTMLProps<HTMLInputElement>>  
-            {...attrs} 
-            customeElement={{
-                ...attrs?.customeElement,
-                end: <IconPercentage />
-             }} 
-             onChange={handleOnChange} value={value}
-        >
-            {
-                (attrsInput) => <input  {...attrsInput} onChange={handleOnChangeFormatedValue} value={value} id={attrsInput?.name} placeholder={attrs?.variant === "v2" ? "" : attrsInput?.placeholder || ""} />
-            }
-        </ContainerInput>
-
-    );
-
-}
-
+    e.target.value = valueFormatted;
+    handleOnChange(e);
+  };
+  return (
+    <ContainerInput<React.HTMLProps<HTMLInputElement>>
+      {...attrs}
+      customeElement={{
+        ...attrs?.customeElement,
+        end: <IconPercentage />,
+      }}
+      onChange={handleOnChange}
+      value={value}
+    >
+      {(attrsInput) => <input {...attrsInput} onChange={handleOnChangeFormatedValue} value={value} id={attrsInput?.name} placeholder={attrs?.variant === "v2" ? "" : attrsInput?.placeholder || ""} />}
+    </ContainerInput>
+  );
+};
 
 export default InputPercentage;
