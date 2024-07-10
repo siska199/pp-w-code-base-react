f6f0442d9a8f9b7561e161998a3d317aad60f3f6
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-07-10 14:38:57 +0700

    update sidebar logic while click menu

--- react/src/components/Sidebar.tsx
+++ react/src/components/Sidebar.tsx
@@ -1,108 +1,80 @@
-import { IconClose } from "@assets/icons"
-import Logo from "@components/ui/Logo"
-import useSidebar from "@hooks/ui/useSidebar"
-import useMediaQuery from "@hooks/useMediaQuery"
-import useMenu from "@hooks/useMenu"
-import { listFlattenMenuSidebar, listGroupingMenuSidebar } from "@lib/utils/data/menu-sidebar"
-import { handleStopPropagation } from "@lib/utils/helper"
-import { useEffect, useState } from "react"
-import NestedMenu, { TMenuSettings } from "./ui/NestedMenu"
+import { IconClose } from "@assets/icons";
+import Logo from "@components/ui/Logo";
+import useSidebar from "@hooks/ui/useSidebar";
+import useMediaQuery from "@hooks/useMediaQuery";
+import useMenu from "@hooks/useMenu";
+import { listFlattenMenuSidebar, listGroupingMenuSidebar } from "@lib/utils/data/menu-sidebar";
+import { handleStopPropagation } from "@lib/utils/helper";
+import { useEffect, useState } from "react";
+import NestedMenu, { TMenuSettings, TParamsOnChangeMenu } from "./ui/NestedMenu";
 
 const Sidebar = () => {
-    const { id, showSidebar, } = useMenu()
-    const { isMaxMd } = useMediaQuery();
-    const { handleToggleSidebar } = useSidebar();
+  const { id, showSidebar } = useMenu();
+  const { isMaxMd } = useMediaQuery();
+  const { handleToggleSidebar } = useSidebar();
 
-    const [topPosition, setTopPosition] = useState(0);
-    const [setting, setSetting] = useState<TMenuSettings>({
-        "0": {
-            customeClass: {
-                label: "text-black font-medium"
-            },
-        },
-        activeMenu: {
-            id: "",
-            level: -1,
-            name: "",
-            parentId: ""
-        },
-        openMenus: {
-            "0-GS0": true,
-            "0-C0": true,
-        }
-    })
-    const [isRender, setIsrender] = useState(false)
+  const [topPosition, setTopPosition] = useState(0);
+  const [setting, setSetting] = useState<TMenuSettings>({
+    "0": {
+      customeClass: {
+        label: "text-black font-medium",
+      },
+    },
+    activeMenu: {
+      id: "",
+      level: -1,
+      name: "",
+      parentId: "",
+    },
+    openMenus: {
+      "0-GS0": true,
+      "0-C0": true,
+    },
+  });
+  const [isRender, setIsrender] = useState(false);
 
-    useEffect(() => {
-        const navbarComp = document.getElementById("navbar")
-        navbarComp&&setTopPosition(navbarComp?.clientHeight)
+  useEffect(() => {
+    const navbarComp = document.getElementById("navbar");
+    navbarComp && setTopPosition(navbarComp?.clientHeight);
 
-        const settingSession = sessionStorage?.getItem('setting')
-        if (settingSession) {
-            const initialSetting = JSON.parse(sessionStorage?.getItem('setting') || "")
-            setting.openMenus = initialSetting.openMenus
-            setting.activeMenu = initialSetting.activeMenu
-        }
-
-        setIsrender(!isRender)
-    }, [])
-
-    useEffect(() => {
-        if (id) {
-            const activeMenu = listFlattenMenuSidebar?.filter((data) => data?.id === id)[0]
-            setting.activeMenu = activeMenu
-            setIsrender(!isRender)
-        }
-    }, [id, location])
-
-    const handleChangeActiveMenu = () => {
-        isMaxMd && handleToggleSidebar()
+    const settingSession = sessionStorage?.getItem("setting");
+    if (settingSession) {
+      const initialSetting = JSON.parse(sessionStorage?.getItem("setting") || "");
+      setting.openMenus = initialSetting.openMenus;
+      setting.activeMenu = initialSetting.activeMenu;
     }
 
-    return (
-        <>
-            <div
-                id="container-sidebar"
-                className=" h-full "
-                onClick={handleToggleSidebar}
-            >
-                <div
-                    id="sidebar"
-                    onClick={handleStopPropagation}
-                    style={{ top: topPosition, }}
-                    className={`fixed overflow-y-hidden h-[calc(100%-3rem)] left-0 bg-white w-0 ${showSidebar !== false && "md:w-[17rem]"}`}
-                >
+    setIsrender(!isRender);
+  }, []);
 
-                    <div
-                        className="p-8 w-full  flex flex-col gap-4 relative  h-full"
-                    >
-                        <div
-                            id="icon-close"
-                            className="hidden justify-between absolute top-6 right-6 cursor-pointer ml-auto"
-                        >
-                            <Logo sizeLogo={"base"} />
-                            <IconClose onClick={handleToggleSidebar} className="ml-auto" />
-                        </div>
-                        <NestedMenu
-                            setting={setting}
-                            setSetting={setSetting}
-                            menu={listGroupingMenuSidebar}
-                            onChangeMenu={handleChangeActiveMenu}
-                        />
-                    </div>
-                </div>
-            </div >
-            <div
-                id="overlay-sidebar"
-                className={`md-overlay min-h-screeen`}
-            />
-        </>
-    )
+  useEffect(() => {
+    if (id) {
+      const activeMenu = listFlattenMenuSidebar?.filter((data) => data?.id === id)[0];
+      setting.activeMenu = activeMenu;
+      setIsrender(!isRender);
+    }
+  }, [id, location]);
 
+  const handleChangeActiveMenu = (params: TParamsOnChangeMenu) => {
+    isMaxMd && params?.groupMenu?.url && handleToggleSidebar();
+  };
 
+  return (
+    <>
+      <div id="container-sidebar" className=" h-full " onClick={handleToggleSidebar}>
+        <div id="sidebar" onClick={handleStopPropagation} style={{ top: topPosition }} className={`fixed overflow-y-hidden h-[calc(100%-3rem)] left-0 bg-white w-0 ${showSidebar !== false && "md:w-[17rem]"}`}>
+          <div className="p-8 w-full  flex flex-col gap-4 relative  h-full">
+            <div id="icon-close" className="hidden justify-between absolute top-6 right-6 cursor-pointer ml-auto">
+              <Logo sizeLogo={"base"} />
+              <IconClose onClick={handleToggleSidebar} className="ml-auto" />
+            </div>
+            <NestedMenu setting={setting} setSetting={setSetting} menu={listGroupingMenuSidebar} onChangeMenu={handleChangeActiveMenu} />
+          </div>
+        </div>
+      </div>
+      <div id="overlay-sidebar" className={`md-overlay min-h-screeen`} />
+    </>
+  );
+};
 
-}
-
-
-
-export default Sidebar
+export default Sidebar;
