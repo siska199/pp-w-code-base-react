9abfa3a7de285b082bd0d80888ca592d5ee9b3d7
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-07-10 21:10:50 +0700

    update content InputRadio

--- react/src/components/module/input-page/input-radio-button/CardAdditionalInfoInputRadioButton.tsx
+++ react/src/components/module/input-page/input-radio-button/CardAdditionalInfoInputRadioButton.tsx
@@ -1,38 +1,32 @@
-
 import CardSubMenu from "@components/cards/CardSubMenu";
 import ProgressStep from "@components/ui/ProgressStep";
+import Filename from "@components/ui/text/Filename";
 
 const CardAdditionalInfoInputRadioButton = () => {
   const listAdditionalInfo = [
     {
-      title: "Handling State for Radio Buttons",
-      caption: "Ensure the state of the selected radio button is managed properly through the `value` prop. Use a state management solution like React's `useState` or a form library to handle state changes and updates."
+      title: "Default Behavior",
+      caption: <>By default, the component renders a group of radio buttons based on the provided &apos;options&apos; prop. It manages the state of the selected radio button through the &apos;value&apos; prop.</>,
     },
     {
-      title: "Custom Styling",
-      caption: "Use the `customeClassnameRadio` prop to apply custom styles to the radio button container and individual radio buttons. This allows for flexibility in styling and aligning the component with your application's design system."
+      title: "Dependencies",
+      caption: (
+        <div>
+          The InputBase component relies on several dependencies and utility functions imported from external sources (
+          <Filename name="ContainerInput" to="/docs/prerequisite/helper-component?id=ContainerInput.tsx" />, ). Ensure these dependencies are properly installed and imported for the component to function correctly.
+          <Filename name="Container" to="/docs/prerequisite/helper-component?id=Container.tsx" />, ). Ensure these dependencies are properly installed and imported for the component to function correctly.
+          <Filename name="TBasePropsInput" to="/docs/prerequisite/types" />, and
+          <Filename name="clsx" importFrom="clsx" />
+        </div>
+      ),
     },
-    {
-      title: "Event Handling",
-      caption: "The `onChange` prop should be used to handle changes to the radio button's value. Ensure that the `onChange` handler updates the state or performs any necessary actions when the radio button state changes."
-    },
-    {
-      title: "Accessibility Considerations",
-      caption: "Ensure that each radio button has a unique `id` and the corresponding label uses the `htmlFor` attribute to enhance accessibility. This helps screen readers correctly associate labels with their respective radio buttons."
-    },
-    {
-      title: "Additional HTML Attributes",
-      caption: "Use the `...attrsInput` spread operator to pass additional HTML attributes to the radio button input element. This can include attributes like `id`, `disabled`, `required`, and more, providing greater control and customization."
-    }
   ];
 
-
   return (
-    <CardSubMenu title={'Additional Info'}>
+    <CardSubMenu title={"Additional Info"}>
       <ProgressStep type="number" listStep={listAdditionalInfo} variant="vertical" />
     </CardSubMenu>
   );
-}
-
+};
 
 export default CardAdditionalInfoInputRadioButton;
--- react/src/components/ui/inputs/InputRadioButton.tsx
+++ react/src/components/ui/inputs/InputRadioButton.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/jsx-handler-names */
-import Container from '@components/ui/Container';
-import ContainerInput from '@components/ui/inputs/ContainerInput';
-import { TBasePropsInput } from '@types';
-import clsx from 'clsx';
-
+import Container from "@components/ui/Container";
+import ContainerInput from "@components/ui/inputs/ContainerInput";
+import { TBasePropsInput } from "@types";
+import clsx from "clsx";
 
 interface TProps extends TBasePropsInput, Omit<React.HTMLProps<HTMLInputElement>, "value" | "onChange"> {
   name: string;
@@ -16,42 +15,40 @@ interface TProps extends TBasePropsInput, Omit<React.HTMLProps<HTMLInputElement>
 }
 
 const InputRadioButton = (props: TProps) => {
-  const { name, onChange, options, value, ...attrsInput } = props
+  const { name, onChange, options, value, ...attrsInput } = props;
 
   return (
     <ContainerInput {...attrsInput} onlyContainer={true}>
       <Container gap={"tiny"}>
-        {
-          options?.map((option, i) => {
-            return (
-              <Container key={i} variant={"hsc"} gap={"base"}>
-                <label htmlFor="check" className="relative flex border items-center rounded-full cursor-pointer">
-                  <input
-                    type="radio"
-                    name={name}
-                    value={option?.value}
-                    className="cursor-pointer bg-priimary peer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity disabled:border-gray-200 disabled:bg-disabled disabled:cursor-not-allowed checked:border-primary  checked:before:bg-primary disabled:before:bg-none disabled:before:opacity-0 hover:before:bg-primary hover:before:opacity-10"
-                    onChange={onChange}
-                    checked={option?.value === value}
-                    {...attrsInput}
-                  />
-                  <div
-                    className={clsx({
-                      "peer-checked:flex hidden absolute mx-auto ml-1 w-3 h-3 rounded-full bg-primary ": true,
-                      "!bg-gray-200": attrsInput?.disabled && option?.value === value
-                    })}
-                  />
-                </label>
-                <label htmlFor="check" className="mt-px cursor-pointer select-none" >
-                  {option?.label}
-                </label>
-              </Container>
-            )
-          })
-        }
+        {options?.map((option, i) => {
+          return (
+            <Container key={i} variant={"hsc"} gap={"base"}>
+              <label htmlFor="check" className="relative flex border items-center rounded-full cursor-pointer">
+                <input
+                  type="radio"
+                  name={name}
+                  value={option?.value}
+                  className="cursor-pointer bg-priimary peer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity disabled:border-gray-200 disabled:bg-disabled disabled:cursor-not-allowed checked:border-primary  checked:before:bg-primary disabled:before:bg-none disabled:before:opacity-0 hover:before:bg-primary hover:before:opacity-10"
+                  onChange={onChange}
+                  checked={option?.value === value}
+                  {...attrsInput}
+                />
+                <div
+                  className={clsx({
+                    "peer-checked:flex hidden absolute mx-auto ml-1 w-3 h-3 rounded-full bg-primary ": true,
+                    "!bg-gray-200": attrsInput?.disabled && option?.value === value,
+                  })}
+                />
+              </label>
+              <label htmlFor="check" className="mt-px cursor-pointer select-none">
+                {option?.label}
+              </label>
+            </Container>
+          );
+        })}
       </Container>
     </ContainerInput>
-  )
-}
+  );
+};
 
-export default InputRadioButton
+export default InputRadioButton;
