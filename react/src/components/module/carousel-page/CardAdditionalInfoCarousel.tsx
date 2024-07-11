be811de863ec9b50b78eda62b373cd7458b5e3cb
SPF <SPF@SM2HONB17D33SPF>
2024-07-11 10:24:48 +0700

    updtae content carouselimage

--- react/src/components/module/carousel-page/CardAdditionalInfoCarousel.tsx
+++ react/src/components/module/carousel-page/CardAdditionalInfoCarousel.tsx
@@ -1,28 +1,58 @@
 import CardSubMenu from "@components/cards/CardSubMenu";
 import ProgressStep from "@components/ui/ProgressStep";
+import Filename from "@components/ui/text/Filename";
 
 const CardAdditionalInfoCarousel = () => {
   const listAdditionalInfo = [
     {
-      title: "Responsive Design",
-      caption: "The carousel adjusts its display based on the screen size. You can configure the number of items visible per view for small, medium, and large screens using the `itemsPerView` prop.",
-    },
-    {
-      title: "Customization",
-      caption: "You can customize the carousel appearance by providing a `className` prop to apply your own CSS styles to the carousel container.",
-    },
-    {
-      title: "Navigation Buttons",
-      caption: "The carousel includes navigation buttons (`IconChevronLeft` and `IconChevronRight`) that allow users to navigate between items. These buttons are automatically disabled when the user is at the beginning or end of the carousel.",
-    },
-    {
-      title: "Group Indicator",
-      caption: "At the bottom of the carousel, there are indicators for each group of items. Clicking on these indicators allows users to jump to different groups of items in the carousel.",
-    },
-    {
-      title: "Dynamic Resize Handling",
-      caption: "The carousel adjusts its item width dynamically based on the container's size. It recalculates item sizes on window resize events to ensure items fit perfectly within the visible area.",
-    },
+      title: "Default Behavior",
+      caption: (
+          <div>
+            The carousel adjusts its display based on the screen size. 
+            You can configure the number of items visible per view for small, medium, and large screens using the &apos;itemsPerView&apos; prop. The navigation buttons (&apos;IconChevronLeft&apos; and &apos;IconChevronRight&apos;) allow users to navigate between items. These buttons are automatically disabled when the user is at the beginning or end of the carousel.
+            At the bottom of the carousel, there are indicators for each group of items. Clicking on these indicators allows users to jump to different groups of items in the carousel.
+          </div>
+      ),
+  },
+  {
+    title: "Dependencies",
+    caption: (
+      <div>
+        The Carousel component relies on several dependencies and utility functions imported from external sources (
+
+        <Filename name="IconChevronRight" to="/docs/prerequisite/icon?name=IconChevronRight" />,
+        <Filename name="IconChevronLeft" to="/docs/prerequisite/icon?name=IconChevronLeft" />,
+        <Filename name="Button" to="/docs/components/button"/>,
+        <Filename name="cn" to="/docs/prerequisite/helper-function?id=cn" />,
+        <Filename name="React" importFrom="react" />,
+        <Filename name="useRef" importFrom="react" />,
+        <Filename name="useEffect" importFrom="react" />, and
+        <Filename name="useState" importFrom="react" />
+        ). Ensure these dependencies are properly installed and imported for the component to function correctly.
+      </div>
+    ),
+  },
+  {
+    title: "Variant",
+    caption: (
+        <div>
+                This component does not have specific variants beyond the props configuration (`className` and `itemsPerView`).
+        </div>
+    ),
+},
+{
+  title: "Usage Notes",
+  caption: (
+      <div>
+          <p>
+              It&apos;s recommended to wrap the &apos;CarouselImage&apos; component in a container with specified dimensions to control its display and responsiveness effectively.
+          </p>
+          <p>
+              Handle the &apos;items&apos; prop carefully to ensure it contains the necessary React nodes for rendering. Unexpected types or empty arrays may lead to rendering issues.
+          </p>
+      </div>
+  ),
+},
   ];
 
   return (
--- react/src/components/module/carousel-page/CardUsageCarousel.tsx
+++ react/src/components/module/carousel-page/CardUsageCarousel.tsx
@@ -11,50 +11,7 @@ const CardUsageCarousel = () => {
   );
 };
 
-const displayUsage = `
-  const dummyImage = [
-    "https://i.pinimg.com/736x/22/54/03/22540381a0bb28c0823dcc0c34a0ad72.jpg",
-    "https://i.pinimg.com/564x/06/b6/ef/06b6ef2a183ee70bfedba85bb24931ea.jpg",
-    "https://i.pinimg.com/736x/39/f0/6c/39f06cf316484b2f585c0ba1bfee8039.jpg",
-    "https://i.pinimg.com/564x/2e/9d/d7/2e9dd78bed90eccb4eeb99bff613c2e8.jpg",
-    "https://i.pinimg.com/564x/f3/b6/db/f3b6dba4edce10df6668ef52068dd486.jpg",
-    "https://i.pinimg.com/564x/cd/2d/47/cd2d47f6c4706e44d81597efdc3101b5.jpg",
-  ]
+const displayUsage ="\n  const dummyImage = [\n    \"https://i.pinimg.com/736x/22/54/03/22540381a0bb28c0823dcc0c34a0ad72.jpg\",\n    \"https://i.pinimg.com/564x/06/b6/ef/06b6ef2a183ee70bfedba85bb24931ea.jpg\",\n    \"https://i.pinimg.com/736x/39/f0/6c/39f06cf316484b2f585c0ba1bfee8039.jpg\",\n    \"https://i.pinimg.com/564x/2e/9d/d7/2e9dd78bed90eccb4eeb99bff613c2e8.jpg\",\n    \"https://i.pinimg.com/564x/f3/b6/db/f3b6dba4edce10df6668ef52068dd486.jpg\",\n    \"https://i.pinimg.com/564x/cd/2d/47/cd2d47f6c4706e44d81597efdc3101b5.jpg\",\n  ]\n\n  return <CarouselImage\n    className=\"h-[30rem]\"\n    itemsPerView={{\n      lg: 3,\n      md: 2,\n      sm: 1\n    }}\n    items={[\n      <Image\n        key={1}\n        src={dummyImage[0]}\n      />,\n      <Image\n        key={2}\n        src={dummyImage[1]}\n      />,\n      <Image\n        key={3}\n        src={dummyImage[2]}\n      />,\n      <Image\n        key={4}\n        src={dummyImage[3]}\n      />,\n      <Image\n        key={5}\n        src={dummyImage[4]}\n      />,\n      <Image\n        key={6}\n        src={dummyImage[5]}\n      />,\n    ]}\n  />\n"
 
-  return <CarouselImage
-    className="h-[30rem]"
-    itemsPerView={{
-      lg: 3,
-      md: 2,
-      sm: 1
-    }}
-    items={[
-      <Image
-        key={1}
-        src={dummyImage[0]}
-      />,
-      <Image
-        key={2}
-        src={dummyImage[1]}
-      />,
-      <Image
-        key={3}
-        src={dummyImage[2]}
-      />,
-      <Image
-        key={4}
-        src={dummyImage[3]}
-      />,
-      <Image
-        key={5}
-        src={dummyImage[4]}
-      />,
-      <Image
-        key={6}
-        src={dummyImage[5]}
-      />,
-    ]}
-  />
-`;
 
 export default CardUsageCarousel;
