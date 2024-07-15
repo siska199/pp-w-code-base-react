62112e7f8cde3db299c8caa57f605d68cdac7fe6
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-07-15 08:54:49 +0700

    update

--- react/src/lib/config/global-variable.ts
+++ react/src/lib/config/global-variable.ts
@@ -1,12 +1,10 @@
+export const secretKey = import.meta.env.VITE_SECRET_KEY;
+export const globalErrorMessage = "Telah terjadi kesalahan pada pada sistem, silahkan contact admin apilikasi";
 
-export const secretKey = import.meta.env.VITE_SECRET_KEY
-export const globalErrorMessage = "Telah terjadi kesalahan pada pada sistem, silahkan contact admin apilikasi"
-
-export const baseURLAPI = import.meta.env.VITE_BASE_URL_API
+export const baseURLAPI = import.meta.env.VITE_BASE_URL_API;
 export const endpoints = {
-    posts: '/posts'
-}
+  posts: "/posts",
+  uploadFile: "https://api.cloudinary.com/v1_1/university-state-of-malang-city/image/upload",
+};
 
-export const pages = {
-    
-}
+export const pages = {};
--- react/src/pages/ExamplesPage.tsx
+++ react/src/pages/ExamplesPage.tsx
@@ -1,11 +1,8 @@
-import Button from "@components/ui/Button";
 import InputFile from "@components/ui/inputs/InputFile";
-import useAPI from "@hooks/useAPI";
 import useForm from "@hooks/useForm";
 import { TFileType } from "@types";
 
 const ExamplesPage = () => {
-  const { apiClient, progress } = useAPI();
   const { form, handleOnChange } = useForm({
     initialForm: {
       "file-input": {
@@ -16,24 +13,10 @@ const ExamplesPage = () => {
     },
   });
 
-  const handleOnSubmit = async () => {
-    const result = await apiClient({
-      endpoint: "https://api.cloudinary.com/v1_1/university-state-of-malang-city/image/upload",
-      isForm: true,
-      payload: {
-        file: form["file-input"]?.value,
-        upload_preset: "my-uploads",
-      },
-    });
-
-    console.log("result: ", result);
-  };
 
   return (
     <div className="h-full">
       <InputFile onChange={handleOnChange} {...form["file-input"]} totalSizeMax={10} listAcceptedFile={[TFileType.JPG, TFileType.XLSX]} />
-      <Button label="Submit File" onClick={handleOnSubmit} />
-      {progress}
     </div>
   );
 };
