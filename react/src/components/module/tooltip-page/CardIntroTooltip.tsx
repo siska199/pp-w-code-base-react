698b4e92da4213b090283bc32bc2fad6626893bd
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-07-16 11:36:32 +0700

    update

--- react/src/components/module/tooltip-page/CardIntroTooltip.tsx
+++ react/src/components/module/tooltip-page/CardIntroTooltip.tsx
@@ -37,6 +37,7 @@ const CardIntroTooltip = () => {
       subTitle="A component tooltip is a user interface element typically used in software applications, websites, or graphical user interfaces (GUIs). Its primary function is to provide additional information or context about a specific UI component when the user interacts with it, usually by hovering over or clicking on the component."
       listExample={listExample}
       displayCodeBase={codeStringComponentUi.Tooltip}
+      customeClass={{tab:{container:"!overflow-visible"}}}
     />
   );
 }
