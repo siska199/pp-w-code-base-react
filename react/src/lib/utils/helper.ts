05358d402d9aa2b6c00cb0a1d48647d5d898cd94
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-07-16 13:56:04 +0700

    update helper function page

--- react/src/lib/utils/helper.ts
+++ react/src/lib/utils/helper.ts
@@ -1,13 +1,15 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const bytesToMegabytes = (bytes: number): number => {
+export function bytesToMegabytes(bytes: number): number {
   return bytes / (1024 * 1024);
-};
+}
 
-export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
 
-export const isEmptyValue = (value: any): boolean => {
+export function isEmptyValue(value: any): boolean {
   if (value === undefined || value === null || value === "") {
     return true;
   }
@@ -18,17 +20,17 @@ export const isEmptyValue = (value: any): boolean => {
     return Object.keys(value).length === 0;
   }
   return false;
-};
+}
 
 interface TParamsSpreadArrayTemp {
   newValue: any;
   array: any[];
 }
-export const spreadArrayAttemp = (params: TParamsSpreadArrayTemp) => {
+export function spreadArrayAttemp(params: TParamsSpreadArrayTemp) {
   const { newValue, array } = params;
 
   return isEmptyValue(array) ? [newValue] : [...array, newValue];
-};
+}
 
 interface TParamsFieldFromObjectList {
   array: any[];
@@ -36,25 +38,25 @@ interface TParamsFieldFromObjectList {
   fieldNameValue: string;
   value: any;
 }
-export const getFieldFromObjectList = (params: TParamsFieldFromObjectList) => {
+export function getFieldFromObjectList(params: TParamsFieldFromObjectList) {
   const { array, fieldNameTarget, fieldNameValue, value } = params;
   return array?.filter((data) => data?.[fieldNameValue] === value)?.[0]?.[fieldNameTarget];
-};
+}
 
-export const getFieldLabelFromOptions = (params: Pick<TParamsFieldFromObjectList, "array" | "value">) => {
+export function getFieldLabelFromOptions(params: Pick<TParamsFieldFromObjectList, "array" | "value">) {
   const { array, value } = params;
   return array?.filter((data) => data?.value === value)?.[0]?.label;
-};
+}
 
 interface TParamsGetAssetURl {
   name: string;
   folder?: "images" | "icons";
 }
 
-export const getAssetURL = (params: TParamsGetAssetURl) => {
+export function getAssetURL(params: TParamsGetAssetURl) {
   const { name, folder = "images" } = params;
   return new URL(`../../assets/${folder}/${name}`, import.meta.url)?.href;
-};
+}
 
 export function checkVisibleElmnInScreen(elm: any) {
   const rect = elm.getBoundingClientRect();
@@ -67,8 +69,12 @@ export function checkElmnOnTopOfPage(elm: any) {
   return rect.top === 0;
 }
 
-export const handleStopPropagation = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => e?.stopPropagation();
-export const handlePreventDefault = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => e?.preventDefault();
+export function handleStopPropagation(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+  return e?.stopPropagation();
+}
+export function handlePreventDefault(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+  return e?.preventDefault();
+}
 
 export function arraysHaveSameMembers(array1: string[], array2: string[]) {
   if (array1.length !== array2.length) {
@@ -89,13 +95,13 @@ const DisplayComponent = () => {
 };`;
 }
 
-export const debounce = (func: (...args: any[]) => void, wait: number) => {
+export function debounce(func: (...args: any[]) => void, wait: number) {
   let timeout: NodeJS.Timeout;
-  return (...args: any[]) => {
+  return function (...args: any[]) {
     clearTimeout(timeout);
     timeout = setTimeout(() => func(...args), wait);
   };
-};
+}
 
 export function findLargestIndexWithValue(arr: any[]) {
   for (let i = arr.length - 1; i >= 0; i--) {
@@ -114,7 +120,7 @@ export function delay(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export const truncateName = (name: string, tooltipWidth: number) => {
-  const maxLength = tooltipWidth / 8; 
+export function truncateName(name: string, tooltipWidth: number) {
+  const maxLength = tooltipWidth / 8;
   return name.length > maxLength ? `${name.slice(0, maxLength)}...` : name;
-};
+}
--- react/src/pages/docs/prerequisite/HelperFunctionPage.tsx
+++ react/src/pages/docs/prerequisite/HelperFunctionPage.tsx
@@ -3,14 +3,16 @@ import CodeBlock from "@components/ui/CodeBlock";
 import Container from "@components/ui/Container";
 import List from "@components/ui/List";
 import ProgressStep from "@components/ui/ProgressStep";
+import { arraysHaveSameMembers, checkElmnOnTopOfPage, checkVisibleElmnInScreen, cn, debounce, findLargestIndexWithValue, findSmallestIndexWithEmptyValue, generateDisplayComponent, getAssetURL, getFieldFromObjectList, getFieldLabelFromOptions, handlePreventDefault, handleStopPropagation, isEmptyValue, spreadArrayAttemp } from "@lib/utils/helper";
 import { TItemList } from "@types";
+
 
 const HelperFunctionPage = () => {
   const listHelper = [
     {
       title: <div id="cn">cn</div>,
       caption: generateCaptionListItemFunction({
-        codeString: codeStringFunction.cn,
+        codeString: cn.toString(),
         purposeDescrition: "Combines multiple class names into a single string, merging Tailwind CSS class names if necessary.",
         listParameter: [{ label: "...inputs", content: ": ClassValue[] - A list of class values." }],
         returnDescription: "A single string with combined class names.",
@@ -19,7 +21,7 @@ const HelperFunctionPage = () => {
     {
       title: <div id="isEmptyValue">isEmptyValue</div>,
       caption: generateCaptionListItemFunction({
-        codeString: codeStringFunction.isEmptyValue,
+        codeString: isEmptyValue.toString(),
         purposeDescrition: "Checks if a value is empty.",
         listParameter: [{ label: "value: any", content: "The value to check." }],
         returnDescription: "A boolean indicating if the value is empty.",
@@ -28,7 +30,7 @@ const HelperFunctionPage = () => {
     {
       title: <div id="spreadArrayAttemp">spreadArrayAttemp</div>,
       caption: generateCaptionListItemFunction({
-        codeString: codeStringFunction.spreadArrayAttemp,
+        codeString: spreadArrayAttemp.toString(),
         purposeDescrition: "Adds a new value to an array, creating a new array if the original is empty.",
         listParameter: [{ label: "params: TParamsSpreadArrayTemp", content: "An object containing the new value and the array." }],
         returnDescription: "A new array with the added value.",
@@ -37,7 +39,7 @@ const HelperFunctionPage = () => {
     {
       title: <div id="getFieldFromObjectList">getFieldFromObjectList</div>,
       caption: generateCaptionListItemFunction({
-        codeString: codeStringFunction.getFieldFromObjectList,
+        codeString: getFieldFromObjectList.toString(),
         purposeDescrition: "Retrieves a field value from an object list based on a matching field value.",
         listParameter: [{ label: "params: TParamsFieldFromObjectList", content: "An object containing the array, target field name, value field name, and value to match." }],
         returnDescription: "The value of the target field from the matched object.",
@@ -46,16 +48,16 @@ const HelperFunctionPage = () => {
     {
       title: <div id="getFieldLabelFromOptions">getFieldLabelFromOptions</div>,
       caption: generateCaptionListItemFunction({
-        codeString: codeStringFunction.getFieldLabelFromOptions,
+        codeString: getFieldLabelFromOptions.toString(),
         purposeDescrition: "Retrieves the label of an option from a list based on a matching value.",
-        listParameter: [{ label: 'params: Pick<TParamsFieldFromObjectList, "array" | "value"> ', content: "An object containing the array and value to match." }],
+        listParameter: [{ label: 'params: Pick<TParamsFieldFromObjectList, "array" | "value">', content: "An object containing the array and value to match." }],
         returnDescription: "The label of the matched option.",
       }),
     },
     {
       title: <div id="getAssetURL">getAssetURL</div>,
       caption: generateCaptionListItemFunction({
-        codeString: codeStringFunction.getAssetURL,
+        codeString: getAssetURL.toString(),
         purposeDescrition: "Generates a URL for an asset file located in the specified folder.",
         listParameter: [{ label: "params: TParamsGetAssetURl", content: 'An object containing the asset name and optional folder (default is "images").' }],
         returnDescription: "The URL of the asset file.",
@@ -64,7 +66,7 @@ const HelperFunctionPage = () => {
     {
       title: <div id="checkVisibleElmnInScreen">checkVisibleElmnInScreen</div>,
       caption: generateCaptionListItemFunction({
-        codeString: codeStringFunction.checkVisibleElmnInScreen,
+        codeString: checkVisibleElmnInScreen.toString(),
         purposeDescrition: "Checks if an element is visible within the viewport.",
         listParameter: [{ label: "elm: any ", content: "The element to check." }],
         returnDescription: "A boolean indicating if the element is visible.",
@@ -73,7 +75,7 @@ const HelperFunctionPage = () => {
     {
       title: <div id="checkElmnOnTopOfPage">checkElmnOnTopOfPage</div>,
       caption: generateCaptionListItemFunction({
-        codeString: codeStringFunction.checkElmnOnTopOfPage,
+        codeString: checkElmnOnTopOfPage.toString(),
         purposeDescrition: "Checks if an element is at the top of the page.",
         listParameter: [{ label: "elm: any ", content: "The element to check." }],
         returnDescription: "A boolean indicating if the element is at the top of the page.",
@@ -82,7 +84,7 @@ const HelperFunctionPage = () => {
     {
       title: <div id="handleStopPropagation">handleStopPropagation</div>,
       caption: generateCaptionListItemFunction({
-        codeString: codeStringFunction.handleStopPropagation,
+        codeString: handleStopPropagation.toString(),
         purposeDescrition: "Stops the propagation of an event.",
         listParameter: [{ label: "e: React.MouseEvent<HTMLDivElement, MouseEvent>", content: "The event to stop propagation for." }],
         returnDescription: "None.",
@@ -91,7 +93,7 @@ const HelperFunctionPage = () => {
     {
       title: <div id="generateDisplayComponent">generateDisplayComponent</div>,
       caption: generateCaptionListItemFunction({
-        codeString: codeStringFunction.generateDisplayComponent,
+        codeString: generateDisplayComponent.toString(),
         purposeDescrition: "Generates a display component string based on the provided component name and dynamic content.",
         listParameter: [
           { label: "ComponentName: string", content: "The name of the component to import and display." },
@@ -103,7 +105,7 @@ const HelperFunctionPage = () => {
     {
       title: <div id="handlePreventDefault">handlePreventDefault</div>,
       caption: generateCaptionListItemFunction({
-        codeString: codeStringFunction.handlePreventDefault,
+        codeString: handlePreventDefault.toString(),
         purposeDescrition: "Prevents the default action of an event.",
         listParameter: [{ label: "e: React.MouseEvent<HTMLDivElement, MouseEvent>", content: "The event to prevent the default action for." }],
         returnDescription: "None.",
@@ -112,7 +114,7 @@ const HelperFunctionPage = () => {
     {
       title: <div id="arraysHaveSameMembers">arraysHaveSameMembers</div>,
       caption: generateCaptionListItemFunction({
-        codeString: codeStringFunction.arraysHaveSameMembers,
+        codeString: arraysHaveSameMembers.toString(),
         purposeDescrition: "Checks if two arrays have the same members.",
         listParameter: [
           { label: "array1: string[]", content: "The first array." },
@@ -124,7 +126,7 @@ const HelperFunctionPage = () => {
     {
       title: <div id="debounce">debounce</div>,
       caption: generateCaptionListItemFunction({
-        codeString: codeStringFunction.debounce,
+        codeString: debounce.toString(),
         purposeDescrition: "Creates a debounced function that delays invoking the provided function until after the specified wait time has passed.",
         listParameter: [
           { label: "func: (...args: any[]) => void", content: "The function to debounce." },
@@ -136,7 +138,7 @@ const HelperFunctionPage = () => {
     {
       title: <div id="findLargestIndexWithValue">findLargestIndexWithValue</div>,
       caption: generateCaptionListItemFunction({
-        codeString: codeStringFunction.findLargestIndexWithValue,
+        codeString: findLargestIndexWithValue.toString(),
         purposeDescrition: "Finds the largest index in an array that has a non-null, non-empty, and non-undefined value.",
         listParameter: [{ label: "arr: any[]", content: "The array to search." }],
         returnDescription: "The largest index with a value, or -1 if no such value exists.",
@@ -145,7 +147,7 @@ const HelperFunctionPage = () => {
     {
       title: <div id="findSmallestIndexWithEmptyValue">findSmallestIndexWithEmptyValue</div>,
       caption: generateCaptionListItemFunction({
-        codeString: codeStringFunction.findSmallestIndexWithEmptyValue,
+        codeString: findSmallestIndexWithEmptyValue.toString(),
         purposeDescrition: "Finds the smallest index in an array that has an empty value.",
         listParameter: [{ label: "arr: any[]", content: "The array to search." }],
         returnDescription: "The smallest index with an empty value, or -1 if no such value exists.",
@@ -167,24 +169,6 @@ interface TParamsGenerateItemFunction {
   listParameter: TItemList[];
   returnDescription: string;
 }
-
-const codeStringFunction = {
-  cn: `import clsx, { ClassValue } from 'clsx';\nimport { twMerge } from 'tailwind-merge';\nexport const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));\n`,
-  isEmptyValue: `export const isEmptyValue = (value: any): boolean => {\n    if (value === undefined || value === null || value === '') {\n        return true;\n    }\n    if (typeof value === 'object') {\n        if (Array.isArray(value)) {\n            return value.length === 0;\n        }\n        return Object.keys(value).length === 0;\n    }\n    return false;\n};`,
-  spreadArrayAttemp: `interface TParamsSpreadArrayTemp {\n    newValue: any;\n    array: any[]\n}\nexport const spreadArrayAttemp = (params: TParamsSpreadArrayTemp) => {\n    const { newValue, array } = params;\n    return isEmptyValue(array) ? [newValue] : [...array, newValue];\n}\n\nexport const isolateEvent = (e: React.MouseEvent<HTMLDivElement | HTMLSpanElement, MouseEvent>) => {\n    e?.preventDefault();\n    e?.stopPropagation();\n};`,
-  getFieldFromObjectList: `interface TParamsFieldFromObjectList {\n    array: any[];\n    fieldNameTarget: string;\n    fieldNameValue: string;\n    value: any;\n}\nexport const getFieldFromObjectList = (params: TParamsFieldFromObjectList) => {\n    const { array, fieldNameTarget, fieldNameValue, value } = params;\n    return array?.filter(data => data?.[fieldNameValue] === value)?.[0]?.[fieldNameTarget];\n};`,
-  getFieldLabelFromOptions: `interface TParamsFieldFromObjectList {\n    array: any[];\n    fieldNameTarget: string;\n    fieldNameValue: string;\n    value: any;\n}\nexport const getFieldLabelFromOptions = (params: Pick<TParamsFieldFromObjectList, "array" | "value">) => {\n    const { array, value } = params;\n    return array?.filter(data => data?.value === value)?.[0]?.label;\n};`,
-  generateDisplayComponent: `export function generateDisplayComponent(ComponentName: string, dynamic: string) {\n    return \`import \${ComponentName} from "@components/ui/\${ComponentName}";\n    \n    const DisplayComponent = () => {\n        \${dynamic};\n    };\`;\n};`,
-  getAssetURL: `interface TParamsGetAssetURl {\n    name: string;\n    folder?: 'images' | 'icons'\n}\n\nexport const getAssetURL = (params: TParamsGetAssetURl) => {\n    const { name, folder = 'images' } = params;\n    return new URL(\`../../assets/\${folder}/\${name}\`, import.meta.url)?.href;\n};`,
-  checkVisibleElmnInScreen: `export function checkVisibleElmnInScreen(elm: any) {\n    const rect = elm.getBoundingClientRect();\n    const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);\n    return !(rect.bottom < 0 || rect.top - viewHeight >= 0);\n};`,
-  checkElmnOnTopOfPage: `export function checkElmnOnTopOfPage(elm: any) {\n    const rect = elm.getBoundingClientRect();\n    return rect.top === 0;\n};`,
-  handleStopPropagation: `export const handleStopPropagation = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => e?.stopPropagation();`,
-  handlePreventDefault: `export const handlePreventDefault = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => e?.preventDefault();`,
-  arraysHaveSameMembers: `export function arraysHaveSameMembers(array1: string[], array2: string[]) {\n    if (array1.length !== array2.length) {\n        return false;\n    }\n    const sortedArray1 = array1.slice().sort();\n    const sortedArray2 = array2.slice().sort();\n    return sortedArray1.every((value, index) => value === sortedArray2[index]);\n};`,
-  debounce: `export const debounce = (func: (...args: any[]) => void, wait: number) => {\n    let timeout: NodeJS.Timeout;\n    return (...args: any[]) => {\n        clearTimeout(timeout);\n        timeout = setTimeout(() => func(...args), wait);\n    };\n};`,
-  findLargestIndexWithValue: `export function findLargestIndexWithValue(arr: any[]) {\n    for (let i = arr.length - 1; i >= 0; i--) {\n        if (arr[i] !== null && arr[i] !== "" && arr[i] !== undefined) {\n            return i;\n        }\n    }\n    return -1;\n};`,
-  findSmallestIndexWithEmptyValue: `export function findSmallestIndexWithEmptyValue(arr: any[]) {\n    return arr.findIndex(value => !value);\n};`,
-};
 
 const generateCaptionListItemFunction = (params: TParamsGenerateItemFunction) => (
   <Container gap={"small"}>
