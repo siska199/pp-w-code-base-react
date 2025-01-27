3703d0ff8c7a662f4ef89302042ebc1ff1982e8a
siska199 <siska.apriana.1703226@students.um.ac.id>
2025-01-27 12:40:54 +0700

    setup: add color design system

---
+++ vue/src/components/hello-world.vue
@@ -0,0 +1,45 @@
+<template>
+    <div :class="{
+        [`${customeClass?.container}`]: customeClass?.container,
+        'd-flex flex-column border bg-secondary-10  gap-2': true,
+    }" :style="{
+        ...customeStyle?.container
+    }">
+        <div :style="{
+            'width': '1.5rem',
+
+            ...customeStyle?.div1
+        }">
+            1
+        </div>
+        <div>
+            2
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue';
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap-vue-next/dist/bootstrap-vue-next.css"
+import "bootstrap-icons/font/bootstrap-icons.css"
+defineProps({
+    customeClass: {
+        type: Object as PropType<{
+            container: string;
+        }>,
+        required: false
+    },
+    customeStyle: {
+        type: Object as PropType<{
+            container?: object;
+            div1?: object;
+        }>,
+        required: false
+    }
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from "node:url"
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(()=>{
+  return {
+    plugins: [vue()],
+    resolve : {
+      alias: {
+          "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    css: {
+        preprocessorOptions: {
+          scss: {
+            quietDeps: true
+          }
+        }
+      }
+  }
 })
